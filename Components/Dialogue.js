a979e580db56d6e218f7f776cbeb9b117820d1b5
Clara Cook <claracoo@buffalo.edu>
2021-11-17 02:16:19 -0500

    added one response

--- Components/Dialogue.js
+++ Components/Dialogue.js
@@ -15,6 +15,10 @@ import SystemMessage from './SystemMessage'
 
 const Dialogue = (props) => {
     const [messages, setMessages] = useState([]);
+    const [trigger, setTrigger] = useState("Feeling Depleted")
+    const [triggers, setTriggers] = useState({"Feeling Depleted": "Why are you feeling depleted?", "My Goals": "Why are you concerned about yout goals?", "Feeling Ambiguity": "In what way are you feeling that there is ambiguity?", "My Fears": "Why do you feel fearful?"})
+    const [msgCount, setMsgCount] = useState(0)
+    const [presetMsgs, setPresetMsgs] = useState(["getFeeling"])
  
     useEffect(() => {
         setMessages([
@@ -29,12 +33,26 @@ const Dialogue = (props) => {
         },
         ])
     }, [])
+
+    function getNextMessage(){
+        let nextMsg = {
+            _id: Date.now(),
+            text: `${triggers[trigger]}`,
+            user: {
+            _id: 2,
+            name: 'Hi Sam',
+            avatar: "https://gm1.ggpht.com/XmdWgDgmAnUERhzVhxFHRVIsJyX4Tcv-_C1BMpXZ74LZ1c1uGH3cBZA3gHBI0Lg0xRK9EwbpB_SrQlqW3ONfCfZc24xqKCCHQ5rfjkVcfQM3VJa-Ml9EpnYu7a6wLJg1bmBkUkKDgm0qo9qfKMAwpYz1QB1lsaiLWjUOHQfHYLI3TLCy7UoFIVQCFpC2r4eqIIt8aMID2RjmgieykD98-UQOzajG5EXgZoW11iNNljZ2U1nZJCbBWKTznrwARWLKsQDpaGq_lCV6P4R3rJQro8m25SathY9arn8FRAf-AeqHc4BjOk6KT3vjhTa22F-n6oCz0HRCpeI8WaRESNphbLT8j6b3vGGcpKrBSiJpwC46CJMEYoNhqu6hqd6nUVxcEVz8Rey_pW5DrnqGJ_eTj1YGkIkcJBRp7M7kI21L7jut30aumK7oHWeaY0lw-nvC6tLfEwqt1FCji6X9tJjyjbRfHGVd_OAySRhokAjSyA22tmLhnms0PRYLk5PDXzickWmFUbGWuaQug1Bs2uHVWYmVbelfytuDz3IGZudeYXqWKuZ4NMIW4PqDgwceTuVIxgNO5Ddo4leK1yA8ZQf0PkjS8ipQrjrTDD7O8tL2gQMOVlsFRuH1La8NFXHE-CSgpHU--9UYzzmpeGKtfrP5jeFmLZeKx2X2vsDqIMvNdjZb47nYsULYJenQyZcZYph88CFBP8cHB3lOqTuwu3aYOfa52M92dTY887c0wMHC=s0-l75-ft-l75-ft",
+            },
+        }
+        return nextMsg
+    }
  
   const onSend = useCallback((messages = []) => {
       let newMessage = messages
      delete newMessage[0].createdAt
-     console.log(newMessage)
-    setMessages(previousMessages => GiftedChat.append(previousMessages, newMessage))
+    let newAndNext = [getNextMessage(), newMessage[0]]
+    setMessages(previousMessages => GiftedChat.append(previousMessages, newAndNext))
+    setMsgCount(msgCount + 1)
   }, [])
  
    return (
