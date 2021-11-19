dd852006714c198169908db5e8a13d569521ed46
Clara Cook <claracoo@buffalo.edu>
2021-11-19 18:12:36 -0500

    styling fixed on landing page

--- Components/Diary.js
+++ Components/Diary.js
@@ -12,7 +12,7 @@ const Diary = (props) => {
     //problem, what I pass down is not cohesive to what gets passed up
    return (
     <View style={{ marginTop: 20, backgroundColor: "#F9F9F9", borderRadius: 10, display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <Text style={{textAlign: "left", marginTop: 20, marginLeft: 70, alignSelf: "flex-start", fontWeight: "bold"}}>My Stories</Text>
+        <Text style={{textAlign: "left", marginTop: 20, marginLeft: 40, alignSelf: "flex-start", fontWeight: "bold"}}>My Stories</Text>
         <View style={{width: 250, justifyContent: "center", alignItems: "center"}}>
             {props.records.map((record, i) => (
                 <DiaryEntry key={i} title={record.title} trigger={record.trigger} date={record.day} story={record.story} why={record.why} colors={props.colors} viewStory={handleViewOldStory}/>
